6a6bd1f767c697caefc33f6b1792037df997dae4
vm <vm4dev@gmail.com>
2025-05-11 21:26:27 +0300

    draft

--- sources/Odyssey.HRMS.Domain.Tests/AutoDataTests.cs
+++ sources/Odyssey.HRMS.Domain.Tests/AutoDataTests.cs
@@ -16,7 +16,7 @@ public sealed class AutoDataTests
         user.Name.Values.Should().NotBeEmpty();
         user.Email.Value.Should().NotBeNullOrEmpty();
     }
-    
+
     [Theory, AutoData]
     public void LeapYearPaidTimeOffTest(User user)
     {
@@ -24,12 +24,26 @@ public sealed class AutoDataTests
         user.Name.Values.Should().NotBeEmpty();
         user.Email.Value.Should().NotBeNullOrEmpty();
     }
-    
+
     [Theory, AutoData]
     public void RegularYearPaidTimeOffTest(User user)
     {
         using var scope = new AssertionScope();
         user.Name.Values.Should().NotBeEmpty();
         user.Email.Value.Should().NotBeNullOrEmpty();
+    }
+
+    [Theory]
+    [InlineData(0, 5, 1)]
+    [InlineData(1, 5, 2)]
+    [InlineData(2, 5, 3)]
+    [InlineData(3, 5, 4)]
+    [InlineData(4, 5, 0)]
+    public void RoundRobinIncrementTest(int initial, int max, int expected)
+    {
+        var previous = initial;
+        var result = Interlocked.Exchange(ref initial, (initial + 1) % max);
+        result.Should().Be(previous);
+        initial.Should().Be(expected);
     }
 }
