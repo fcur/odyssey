0636952b65b10167a3bfd9f8742680fd3e5cf6a3
vm <vm4dev@gmail.com>
2025-09-07 11:53:01 +0300

    checkpoint

--- sources/Odyssey.HRMS.EventLogLite.Tests/FileEventLogBrokerTests.cs
+++ sources/Odyssey.HRMS.EventLogLite.Tests/FileEventLogBrokerTests.cs
@@ -88,35 +88,46 @@ public sealed class FileEventLogBrokerTests : IAsyncLifetime, IClassFixture<File
         var workingDirectory = LogSegmentDirectory.Init(topic);
         var logSegment1 = FileLogSegment.New(partition, workingDirectory) with { BaseOffset = baseOffset1 };
         var logSegment2 = FileLogSegment.New(partition, workingDirectory) with { BaseOffset = baseOffset2 };
-        
-        var time0 = now.AddMinutes(-8.0).ToUnixTimeMilliseconds();
-        var time1 = now.AddMinutes(-2.0).ToUnixTimeMilliseconds();
-        var time2 = now.AddMinutes(1.0).ToUnixTimeMilliseconds();
-        var logMessage0 = LogMessage<TestEvent>.Create(key1, payload1, baseOffset1) with { Timestamp = time0 };
-        var logMessage1 = LogMessage<TestEvent>.Create(key1, payload1, baseOffset2) with { Timestamp = time1 };
-        var logMessage2 = LogMessage<TestEvent>.Create(key2, payload2, baseOffset2 + 1) with { Timestamp = time2 };
+
+        var time1 = now.AddMinutes(-8.0).ToUnixTimeMilliseconds();
+        var time2 = now.AddMinutes(-2.0).ToUnixTimeMilliseconds();
+        var time3 = now.AddMinutes(1.0).ToUnixTimeMilliseconds();
+
+        var logMessage1 = LogMessage<TestEvent>.Create(key1, payload1, baseOffset1) with { Timestamp = time1 };
+        var logMessage2 = LogMessage<TestEvent>.Create(key1, payload1, baseOffset2) with { Timestamp = time2 };
+        var logMessage3 = LogMessage<TestEvent>.Create(key2, payload2, baseOffset2 + 1) with { Timestamp = time3 };
 
         _fixture.CreateEmptyLogSegments([logSegment1, logSegment2]);
-        var logSegmentResult1 = await _fixture.Write(logSegment1, [logMessage0], cts.Token);
-        var logSegmentResult2 = await _fixture.Write(logSegment2, [logMessage1, logMessage2], cts.Token);
-        var logSegments = LogSegmentDirectory.Scan(topic.Name);
+        var logSegmentResult1 = await _fixture.Write(logSegment1, [logMessage1], cts.Token);
+        var logSegmentResult2 = await _fixture.Write(logSegment2, [logMessage2, logMessage3], cts.Token);
+        var logSegmentScanResult = LogSegmentDirectory.Scan(topic.Name).ToArray();
+
+        var activeSegment = logSegmentScanResult.Length == 2 ? logSegmentScanResult[0] : throw new InvalidOperationException();
+        var notActiveSegment = logSegmentScanResult.Length == 2 ? logSegmentScanResult[1] : throw new InvalidOperationException();
 
         LogSegmentDirectory.Cleanup(topic.Name);
 
         using var scope = new AssertionScope();
+
+        logSegmentResult1.Should().NotBeNull();
+        logSegmentResult1.BaseOffset.Should().Be(logMessage1.Offset);
+        logSegmentResult1.BaseTime.Should().Be(logMessage1.Timestamp);
+
         logSegmentResult2.Should().NotBeNull();
-        logSegmentResult2.BaseOffset.Should().Be(logMessage1.Offset);
-        logSegmentResult2.BaseTime.Should().Be(logMessage1.Timestamp);
-        logSegmentResult2.IsActive.Should().BeTrue();
+        logSegmentResult2.BaseOffset.Should().Be(logMessage2.Offset);
+        logSegmentResult2.BaseTime.Should().Be(logMessage2.Timestamp);
 
-        logSegments.Should().ContainSingle();
-        logSegmentScanResult.Should().NotBeNull();
-        logSegmentScanResult!.BaseOffset.Should().Be(logMessage1.Offset);
-        logSegmentScanResult.BaseTime.Should().Be(logMessage1.Timestamp);
-        logSegmentScanResult.IsActive.Should().BeTrue();
+        activeSegment.Should().NotBeNull();
+        activeSegment.BaseOffset.Should().Be(logMessage2.Offset);
+        activeSegment.BaseTime.Should().Be(logMessage2.Timestamp);
+        activeSegment.IsActive.Should().BeTrue();
+
+        notActiveSegment.Should().NotBeNull();
+        notActiveSegment.BaseOffset.Should().Be(logMessage1.Offset);
+        notActiveSegment.BaseTime.Should().Be(logMessage1.Timestamp);
+        notActiveSegment.IsActive.Should().BeFalse();
     }
-
-
+    
     [Theory, AutoData]
     public async Task TestLogEvent(string key, TestEvent payload)
     {
